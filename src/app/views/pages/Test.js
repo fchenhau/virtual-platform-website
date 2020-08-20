d6fc5c66f1355196c3578f7b8395822de928d656
FungCH <chenhau.fung@worldcloudventures.com>
2020-08-20 22:07:01 +0800

    Update: test parallax page

--- src/app/views/pages/Test.js
+++ src/app/views/pages/Test.js
@@ -13,11 +13,16 @@ const Test = () => {
                     <button onClick={() => refParallax.current.scrollTo(1)}>Scroll to page 2</button>
                 </div>
             </ParallaxLayer>
+            <ParallaxLayer offset={0.4} speed={-0.2} style={{ backgroundColor: '#ff0'}}>
+                <img src={"https://picsum.photos/300/200"} style={{ position: "absolute", left: "20%", top: "0"}}/>
+            </ParallaxLayer>
             <ParallaxLayer offset={1} speed={0.5} style={{ backgroundColor: 'green'}}>
                 <div>
                     <img src={"https://picsum.photos/300/200"} />
                     <button onClick={() => refParallax.current.scrollTo(2)}>Scroll to page 3</button>
                 </div>
+            </ParallaxLayer>
+            <ParallaxLayer offset={1.6} speed={0.5} style={{ backgroundColor: '#0ff'}}>
             </ParallaxLayer>
             <ParallaxLayer offset={2} speed={0.5} style={{ backgroundColor: 'blue'}}>
                 <div>
@@ -26,8 +31,14 @@ const Test = () => {
                 </div>
             </ParallaxLayer>
 
-            <ParallaxLayer offset={1.6} speed={-0.1} style={{ pointerEvents: 'none' }}>
+            <ParallaxLayer offset={1.6} speed={-0.3} style={{ pointerEvents: 'none' }}>
                 <img src={"https://picsum.photos/200/200"} />
+            </ParallaxLayer>
+            <ParallaxLayer offset={0.8} speed={0.2} style={{ pointerEvents: 'none' }}>
+                <img src={"https://picsum.photos/200/200"} style={{ position: "absolute", right: "20%", bottom: "40%"}} />
+            </ParallaxLayer>
+            <ParallaxLayer offset={1.3} speed={1} style={{ pointerEvents: 'none' }}>
+                <img src={"https://picsum.photos/100/200"} style={{ position: "absolute", right: "10%", top: "30%"}} />
             </ParallaxLayer>
         </Parallax>
     )
