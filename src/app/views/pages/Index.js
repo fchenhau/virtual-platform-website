c43806c6b464911465826c568c66556188d182bd
Fung Chen Hau <chenhau96@hotmail.com>
2020-08-24 23:37:13 +0800

    Add: main page

--- src/app/views/pages/Index.js
+++ src/app/views/pages/Index.js
@@ -1,70 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { exampleTypes, exampleActions } from "../../states/example";
 import { withRouter } from 'react-router-dom'
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
+
+import Main from "./public/Main"
 
 const Index = ({ history }) => {
 
-    const dispatch = useDispatch();
-    const resetGetRequest = () => dispatch(exampleActions.resetGetRequest());
-    const resetPostRequest = () => dispatch(exampleActions.resetPostRequest());
-
-    const getRequestResponse = useSelector(({ example }) => example.getRequestResponse);
-    const postRequestResponse = useSelector(({ example }) => example.postRequestResponse);
-
-    const [getRequestState, setGetRequestState] = useState(null);
-    const [postRequestState, setPostRequestState] = useState(null);
-
-    useEffect(() => {
-        dispatch(exampleActions.getRequest());
-        dispatch(exampleActions.postRequest());
-    }, []);
-
-    useEffect(() => {
-        const { type, message } = getRequestResponse;
-        
-        if (type === exampleTypes.GET_REQUEST_SUCCESS) {
-            resetGetRequest();
-            setGetRequestState(message);
-           
-        }
-
-        if (type === exampleTypes.GET_REQUEST_FAIL) {
-            resetGetRequest();
-            setGetRequestState(message);
-        }
-
-    }, [getRequestResponse]);
-
-    useEffect(() => {
-        const { type, message } = postRequestResponse;
-
-        if (type === exampleTypes.POST_REQUEST_SUCCESS) {
-            resetPostRequest();
-            setPostRequestState(message);
-        }
-
-        if (type === exampleTypes.POST_REQUEST_FAIL) {
-            resetPostRequest();
-            setPostRequestState(message);
-        }
-
-    }, [postRequestResponse]);
+    const refParallax = useRef(null);
 
     return (
-        <div>
-            <h1>Home Page</h1>
-
-            <div>
-                <p>Initial state for getRequestResponse: {getRequestResponse.type}</p>
-                <p>Initial state for postRequestResponse: {postRequestResponse.type}</p>
-            </div>
-
-            <div>
-                <p>Message for getRequestResponse: {getRequestState}</p>
-                <p>Message for postRequestResponse: {postRequestState}</p>
-            </div>
-        </div>
+        <Parallax pages={3} scrolling={true} horizontal={false} ref={refParallax}>
+            <Main />
+        </Parallax>
     )
 };
 
---
+++ src/app/views/pages/public/Main.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useRef } from 'react'
+import MainLogo from "../../../assets/images/main_logo.png"
+import "../../../styles/main.css"
+
+const Main = () => {
+
+    return (
+        <div className="main-page">
+
+            <div className="logo">
+                <img src={MainLogo} alt="MDM 2020 Virtual Logo" 
+                    className="img img-fluid" width="25%" />
+            </div>
+            
+            <div className="logo-text w-100">
+                <div className="title text-white px-lg-4">
+                    <h5 className="mb-0">DIGITAL SKILLS TODAY,</h5>
+                    <h5 className="mb-0">WORKPLACE OF TOMORROW</h5>
+                </div>
+
+                <div className="borderline">
+                </div>
+
+                <span className="subtitle text-white px-lg-4">
+                    RAISING DIGITAL HEROES
+                </span>
+            </div>
+
+            <div className="stats w-100">
+                <div className="d-flex justify-content-around align-items-center flex-wrap text-white">
+                    <div className="d-flex flex-column justify-content-center align-items-center px-2 mb-2">
+                        <span>>40</span>
+                        <span>VIRTUAL EXHIBITORS</span>
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-center px-2 mb-2">
+                        <span>>30</span>
+                        <span>SPEAKERs</span>
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-center px-2 mb-2">
+                        <span>>30</span>
+                        <span>WORKSHOPS & TUTORIALS</span>
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-center px-2 mb-2">
+                        <span>10</span>
+                        <span>COMPETITIONS</span>
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-center px-2 mb-2">
+                        <span>>3,000</span>
+                        <span>DIGITAL TECH JOBS</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default (Main);
