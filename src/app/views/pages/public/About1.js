1991a9d13bf3129e6b1d663cb0fbdf698e533ce2
Fung Chen Hau <chenhau96@hotmail.com>
2020-08-25 23:13:38 +0800

    Add text for about page 1

--- src/app/views/pages/public/About1.js
+++ src/app/views/pages/public/About1.js
@@ -6,10 +6,24 @@ const About1 = () => {
 
     return (
         <div className="about-page-1">
+            <div className="about-page-1-text">
+                <h3>RAISING DIGITAL HEROES</h3>
+                <p>
+                    The #mydigitalmaker Virtual Fair 2020 is the flagship event of the #mydigitalmaker Movement since 2017. 
+                    The Fair celebrates what the movement has accomplished throughout the year. 
+                    It is also to encourage Malaysian youths to grow from digital users to digital innovators.
+                </p>
+                <p>
+                    The #mydigitalmaker Virtual Fair 2020 is the premier digital tech education exhibition in the region!
+                </p>
+            </div>
+
             <div className="d-flex align-items-center h-100">
                 <img src={RaisingDigitalHeroesImage} alt="Raising Digital Heroes" 
                     className="img img-fluid" width="100%"/>
             </div>
+
+            
         </div>
     )
 };
