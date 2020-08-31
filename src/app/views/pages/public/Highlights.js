bc5e4c7d50a6fb042da82a9ba7ca0717fdb112e6
Fung Chen Hau <chenhau96@hotmail.com>
2020-08-31 21:09:02 +0800

    Add styling for highlights

--- src/app/views/pages/public/Highlights.js
+++ src/app/views/pages/public/Highlights.js
@@ -14,13 +14,13 @@ const Highlights = () => {
         <div id="highlights">
 
             <div className="d-flex flex-column justify-content-center h-100 px-4">
-                <h3 className="px-3">FOCUS PILLAR</h3>
+                <h1 className="text-header px-3">FOCUS PILLAR</h1>
 
-                <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex flex-md-row justify-content-between align-items-center">
                     <div className="d-flex flex-column align-items-center h-100">
                         <img src={HighlightImg1} alt="EXTENDED REALITY" 
                             className="img img-fluid p-3" />
-                        <h6 className="text-center">EXTENDED REALITY</h6>
+                        <h6 className="text-header text-center">EXTENDED REALITY</h6>
                         <div className="text-center">
                             <div className="text-muted">Virtual Reality</div>
                             <div className="text-muted">Augmented Reality</div>
@@ -31,43 +31,43 @@ const Highlights = () => {
                     <div className="d-flex flex-column align-items-center h-100">
                         <img src={HighlightImg2} alt="ARTIFICIAL INTELLIGENCE" 
                             className="img img-fluid p-3" />
-                        <h6 className="text-center">ARTIFICIAL INTELLIGENCE</h6>
+                        <h6 className="text-header text-center">ARTIFICIAL INTELLIGENCE</h6>
                         <div className="text-center">
-                            <div>Machine Learning</div>
-                            <div>Deep Learning</div>
-                            <div>Big Data</div>
+                            <div className="text-muted">Machine Learning</div>
+                            <div className="text-muted">Deep Learning</div>
+                            <div className="text-muted">Big Data</div>
                         </div>
                     </div>
 
                     <div className="d-flex flex-column align-items-center h-100">
                         <img src={HighlightImg3} alt="ROBOTICS" 
                             className="img img-fluid p-3" />
-                        <h6 className="text-center">ROBOTICS</h6>
+                        <h6 className="text-header text-center">ROBOTICS</h6>
                         <div className="text-center">
-                            <div>Automations</div>
-                            <div>Drones</div>
-                            <div>Rapid Prototyping</div>
-                            <div>Cloud/IOT</div>
+                            <div className="text-muted">Automations</div>
+                            <div className="text-muted">Drones</div>
+                            <div className="text-muted">Rapid Prototyping</div>
+                            <div className="text-muted">Cloud/IOT</div>
                         </div>
                     </div>
 
                     <div className="d-flex flex-column align-items-center h-100">
                         <img src={HighlightImg4} alt="DIGITAL ARTS" 
                             className="img img-fluid p-3" />
-                        <h6 className="text-center">DIGITAL ARTS</h6>
+                        <h6 className="text-header text-center">DIGITAL ARTS</h6>
                         <div className="text-center">
-                            <div>3D Modeling</div>
-                            <div>Graphic Design</div>
-                            <div>Animation</div>
+                            <div className="text-muted">3D Modeling</div>
+                            <div className="text-muted">Graphic Design</div>
+                            <div className="text-muted">Animation</div>
                         </div>
                     </div>
 
                     <div className="d-flex flex-column align-items-center h-100">
                         <img src={HighlightImg5} alt="DIGITAL COMMUNITY" 
                             className="img img-fluid p-3" />
-                        <h6 className="text-center">DIGITAL COMMUNITY</h6>
+                        <h6 className="text-header text-center">DIGITAL COMMUNITY</h6>
                         <div className="text-center">
-                            <div>Celebratin the efforts by the digital maker communities during COVID-19</div>
+                            <div className="text-muted">Celebratin the efforts by the digital maker communities during COVID-19</div>
                         </div>
                     </div>
                 </div>
