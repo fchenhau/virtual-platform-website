a5e5d83a51f732be67d8a9eb02498a7f3f64e629
Fung Chen Hau <chenhau96@hotmail.com>
2020-08-31 17:01:39 +0800

    Add burger menu

--- src/app/views/pages/Index.js
+++ src/app/views/pages/Index.js
@@ -11,11 +11,15 @@ import Speakers from "./public/Speakers"
 import Login from "./public/Login"
 import Register from "./public/Register"
 
-import BurgerMenuImg from "../../assets/images/burger_menu.png"
-import AboutFairMenuImg from "../../assets/images/menu_about_fair.png"
-import HighlightMenuImg from "../../assets/images/menu_highlight.png"
-import SpeakersMenuImg from "../../assets/images/menu_speakers.png"
-import RegistrationMenuImg from "../../assets/images/menu_registration.png"
+import BurgerMenuImg from "../../assets/images/Menu/burger_menu.png"
+import AboutFairMenuImg from "../../assets/images/Menu/menu_about_fair.png"
+import HighlightsMenuImg from "../../assets/images/Menu/menu_highlights.png"
+import SpeakersMenuImg from "../../assets/images/Menu/menu_speakers.png"
+import RegistrationMenuImg from "../../assets/images/Menu/menu_registration.png"
+import AboutFairImg from "../../assets/images/Menu/about_fair.png"
+import HighlightsImg from "../../assets/images/Menu/highlights.png"
+import SpeakersImg from "../../assets/images/Menu/speakers.png"
+import RegistrationImg from "../../assets/images/Menu/registration.png"
 
 const pagesOffset = {
     'main': 0,
@@ -27,14 +31,18 @@ const pagesOffset = {
     'register': 4
 }
 
+const scrollOffset = {
+    'about1': 313 - 30,
+    'highlights': 939 - 30,
+    'speakers': 1252 - 30,
+    'login': 1877 - 30,
+}
+
 const Index = ({ history }) => {
 
-    const refParallax = useRef(null);
+    const refParallax = useRef();
 
-    const handleOnClickRegister = () => {
-        refParallax.current.scrollTo(4);
-    }
-
+    const [currOffset, setCurrOffset] = useState(0);
     const [isMenuToggle, setIsMenuToggle] = useState(false);
 
     const toggleMenu = () => {
@@ -43,7 +51,20 @@ const Index = ({ history }) => {
 
     const scrollTo = (page) => {
         refParallax.current.scrollTo(pagesOffset[page]);
+
+        // let newOffset = scrollOffset[page];
+        // setCurrOffset(newOffset);
     }
+
+    useEffect(() => {
+        const interval = setInterval(function() { 
+            setCurrOffset(refParallax.current.current);
+        }, 500);
+        
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
 
     return (
         <>
@@ -57,25 +78,49 @@ const Index = ({ history }) => {
                     isMenuToggle
                     &&
                     <ul id="nav_menu" className="d-flex flex-column">
-                        <li className="mb-2" onClick={() => scrollTo('about1')}>
-                            <img src={AboutFairMenuImg} alt="About Fair" className="nav_menu_item" />
+                        <li className="mb-3" onClick={() => scrollTo('about1')}>
+                            {
+                                currOffset >= scrollOffset.about1 && currOffset < scrollOffset.highlights
+                                ?
+                                <img src={AboutFairMenuImg} alt="About Fair" className="nav_menu_item_active" />
+                                :
+                                <img src={AboutFairImg} alt="About Fair" className="nav_menu_item" />
+                            }
                         </li>
-                        <li className="mb-2" onClick={() => scrollTo('highlights')}>
-                            <img src={HighlightMenuImg} alt="Highlights" className="nav_menu_item" />
+                        <li className="mb-3" onClick={() => scrollTo('highlights')}>
+                            {
+                                currOffset >= scrollOffset.highlights && currOffset < scrollOffset.speakers
+                                ?
+                                <img src={HighlightsMenuImg} alt="Highlights" className="nav_menu_item_active" />
+                                :
+                                <img src={HighlightsImg} alt="About Fair" className="nav_menu_item" />
+                            }
                         </li>
-                        <li className="mb-2" onClick={() => scrollTo('speakers')}>
-                            <img src={SpeakersMenuImg} alt="Speakers" className="nav_menu_item" />
+                        <li className="mb-3" onClick={() => scrollTo('speakers')}>
+                            {
+                                currOffset >= scrollOffset.speakers && currOffset < scrollOffset.login
+                                ?
+                                <img src={SpeakersMenuImg} alt="Speakers" className="nav_menu_item_active" />
+                                :
+                                <img src={SpeakersImg} alt="Speakers" className="nav_menu_item" />
+                            }
                         </li>
-                        <li className="mb-2" onClick={() => scrollTo('login')}>
-                            <img src={RegistrationMenuImg} alt="Registration" className="nav_menu_item" />
+                        <li className="mb-3" onClick={() => scrollTo('login')}>
+                            {
+                                currOffset >= scrollOffset.login
+                                ?
+                                <img src={RegistrationMenuImg} alt="Registration" className="nav_menu_item_active" />
+                                :
+                                <img src={RegistrationImg} alt="Registration" className="nav_menu_item" />
+                            }
                         </li>
                     </ul>
                 }
                 
             </div>
 
-            <Parallax pages={5} scrolling={true} horizontal={false} ref={refParallax}>
 
+            <Parallax pages={5} scrolling={true} horizontal={false} ref={refParallax}>
 
                 {/* Static Layers */}
                 <ParallaxLayer offset={0} speed={0}>
@@ -93,7 +138,7 @@ const Index = ({ history }) => {
 
                 <ParallaxLayer offset={3} speed={0}>
                     <Login 
-                        onClickRegister={handleOnClickRegister} 
+                        onClickRegister={() => scrollTo('register')} 
                     />
                 </ParallaxLayer>
 
